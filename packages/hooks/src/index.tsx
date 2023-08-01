8e0f2ac962a22a964800108e4a4686decca3a4bd
wangxinyu <wangxinyu@datangqian.com>
2023-08-01 16:51:35 +0800

    feat: 更新

--- packages/hooks/src/index.tsx
+++ packages/hooks/src/index.tsx
@@ -2,33 +2,3 @@
 export { default as useSwitch } from './useSwitch';
 import { default as useLifeCycle } from './useLifeCycle';
 
-import React, { useState, useCallback } from 'react';
-
-export const Test = () => {
-  const [value, setValue] = useState(0)
-  const onMount = useCallback(() => {
-    console.log(value);
-  }, [value]);
-  const onUnmount = useCallback(() => {
-    console.log(value);
-  }, [value]);
-  const { isUnmount } = useLifeCycle({
-    onMount,
-    onUnmount,
-  })
-  return <p onClick={() => setValue(i => i + 1)}>ADD {value} {isUnmount ? 'true' : 'false'}</p>;
-};
-export const Demos = () => {
-  const [bool, setBool] = useState(true);
-  return (
-    <div>
-      <div onClick={ () => setBool(false) }>
-        Hidden
-      </div>
-      {
-        bool
-          ? <Test />
-          : undefined}
-    </div>
-  );
-};
