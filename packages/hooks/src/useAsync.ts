fd948d8437fbb55845fc1ec7031e9a1e2a678b78
wangxinyu <wangxinyu@datangqian.com>
2023-09-11 18:20:56 +0800

    feat: async

--- packages/hooks/src/useAsync.ts
+++ packages/hooks/src/useAsync.ts
@@ -1,34 +1,49 @@
 
-import useSafeState from "./useSafeState";
-import useRtCb from "./useRtCb";
+import useSafeState from './useSafeState';
+import useRtCb from './useRtCb';
+import useCreate from './useCreate';
 
-interface Opts<T extends (...arg: any) => any, R> {
-  defParams: Parameters<T>;
-  format: (p: ReturnType<T>) => R;
+interface Opt<T extends (...arg: any) => any, R> {
+  defParam?: Parameters<T>;
+  immediate?: boolean;
+  format?: (p: ReturnType<T>) => R;
+  catchParam?: boolean;
 }
 
 const useAsync = <
   T extends (...arg: any) => any,
   R extends Object
->(cb: T, opts: Opts<T, R>) => {
-
+>(asyncFn: T, opt: Opt<T, R> = {
+  immediate: false,
+  catchParam: false,
+}) => {
   const [data, setData] = useSafeState(null);
-  const [params, setParams] = useSafeState(opts?.defParams);
+  const [param, setParam] = useSafeState(opt.defParam || {});
   const [loading, setLoading] = useSafeState(false);
   const [error, setError] = useSafeState(null);
-  const run = useRtCb((runParams) => {
-    const currParams = {
-      ...params,
-      ...runParams,
-    }
-    setLoading(true)
-    return cb(currParams).then(opts?.format).then((resData) => {
-      setData(resData);
-      setParams(currParams);
-    }, setError).finally(() => {
-      setLoading(false)
-    })
+
+  const run = useRtCb((runParam: Partial<Parameters<T>>) => {
+    const currParam = opt.catchParam ? {
+      ...param,
+      ...runParam,
+    } : runParam;
+    setLoading(true);
+    setError(null);
+    return asyncFn(currParam)
+      .then(opt.format)
+      .then((resData) => {
+        setData(resData);
+        setParam(currParam);
+      }, setError)
+      .finally(() => {
+        setLoading(false)
+      })
   })
+  useCreate(() => {
+    if (opt.immediate) {
+      run(param);
+    }
+  });
   return {
     data,
     run,
