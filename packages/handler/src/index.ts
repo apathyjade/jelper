8b9d4b83e8d8f80a57e11ff77b4caf3d4009ac99
wangxinyu <wangxinyu@datangqian.com>
2024-01-31 11:31:06 +0800

    feat: 优化

--- packages/handler/src/index.ts
+++ packages/handler/src/index.ts
@@ -1,29 +1,35 @@
 
-import CallbackCache from './CallbackCache';
-
 type PromiseResult<T extends Promise<any>> = T extends Promise<infer R> ? R : never
 
 export default class Handler<T extends (p: any) => Promise<any>> {
-  cache: CallbackCache<T>;
   constructor() {
-    this.cache = new CallbackCache();
+    this.value = [];
+  }
+  private value: T[]
+  get size(): number {
+    return this.value.length;
   }
   on(cb: T) {
-    this.cache.add(cb);
+    this.value = [ ...this.value, cb ];
   }
   off(cb: T) {
-    this.cache.delete(cb);
+    this.value = this.value.filter((item) => item !== cb);
+  }
+  clear() {
+    this.value = [];
   }
   dispose() {
-    this.cache.clear();
-    this.cache = null;
+    this.clear();
+    this.value = null;
   }
   call(data: Parameters<T>): Promise<PromiseResult<ReturnType<T>>[]> {
-    const cbs = this.cache.value;
+    const cbs = this.value;
     return <Promise<PromiseResult<ReturnType<T>>[]>>Promise.all(cbs.map(async(cb) => await cb(data)));
   }
   callWithRace(data: Parameters<T>): ReturnType<T> {
-    const cbs = this.cache.value;
+    const cbs = this.value;
     return <ReturnType<T>>Promise.race(cbs.map(async(cb) => await cb(data)));
   }
 }
+
+export const useHandler = () => new Handler();
