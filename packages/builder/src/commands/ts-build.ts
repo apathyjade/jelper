66162ae32aa27336625394fa6ebd2b1a7182f619
jade <apathyjade@outlook.com>
2024-06-11 18:06:15 +0800

    feat: 配置target

--- packages/builder/src/commands/ts-build.ts
+++ packages/builder/src/commands/ts-build.ts
@@ -3,6 +3,6 @@ import { $ } from 'execa';
 import { resolveByBasePath } from '../common/index.js';
 
 export default () => {
-  $`tsc -p ./ --module es6 --outDir ${resolveByBasePath('es')}`;
-  $`tsc -p ./ --module commonjs --outDir ${resolveByBasePath('lib')}`;
+  $`tsc -p ./ --target es6 --module es6 --outDir ${resolveByBasePath('es')}`;
+  $`tsc -p ./ --target es5 --module commonjs --outDir ${resolveByBasePath('lib')}`;
 }
