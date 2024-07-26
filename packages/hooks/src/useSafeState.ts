77c1e55959c132515ba8cbbe4e96d3751623fddb
jade <apathyjade@outlook.com>
2024-07-26 16:25:46 +0800

    feat: ts类型优化

--- packages/hooks/src/useSafeState.ts
+++ packages/hooks/src/useSafeState.ts
@@ -3,11 +3,12 @@ import { useState } from 'react';
 import useSafeCb from './useSafeCb';
 
 const useSafeState = <T>(value?: T): [
-    s: T|undefined,
-    setFn: React.Dispatch<React.SetStateAction<T|undefined>>,
+    s: T,
+    setFn: React.Dispatch<React.SetStateAction<T>>,
 ] => {
-    const [state, setState] = useState<T|undefined>(value);
+    const [state, setState] = useState<T>(value as any);
     const setCb = useSafeCb(setState)
     return [state, setCb];
 }
 export default useSafeState;
+
