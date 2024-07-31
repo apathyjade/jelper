f560e00ea5864911e49d857a8b9867ea7b0ff9b0
jade <apathyjade@outlook.com>
2024-07-31 17:46:20 +0800

    feat: 优化

--- packages/hooks/src/useBoolState.ts
+++ packages/hooks/src/useBoolState.ts
@@ -5,10 +5,12 @@ import useSafeState from './useSafeState';
 const useBoolState = (value: boolean) => {
   const [val, setVal] = useSafeState<boolean>(value);
   const switchFn = useCallback((switchValue?: React.SetStateAction<boolean>) => {
-    if (typeof switchValue === 'boolean') {
-      setVal(switchValue)
-    } else {
+    if (switchValue === undefined || switchValue === null) {
       setVal((oldVal) => !oldVal);
+    } else if (typeof switchValue === 'boolean' || typeof switchValue === 'function') {
+      setVal(switchValue);
+    } else {
+      setVal(!!switchValue);
     }
   }, [])
   return [val, switchFn];
--- packages/hooks/src/useRtCb.ts
+++ packages/hooks/src/useRtCb.ts
@@ -4,8 +4,7 @@ import useRtRef from './useRtRef';
 
 const useRtCb = <
   T extends DefFn,
-  D extends any[]
->(cb: T, deps?: D): ReturnFn<T> => {
+>(cb: T, deps?: any[]): ReturnFn<T> => {
   const ref = useRtRef<T>(cb);
   return useSafeCb((...arg: Parameters<T>[]) => {
     return ref.current?.(...arg);
