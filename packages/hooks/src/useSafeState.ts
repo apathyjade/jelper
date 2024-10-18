39233e0c48945287d15763a43b4457653438501a
jade <apathyjade@outlook.com>
2024-10-18 18:21:47 +0800

    feat: 优化

--- packages/hooks/src/useSafeState.ts
+++ packages/hooks/src/useSafeState.ts
@@ -6,7 +6,7 @@ const useSafeState = <T>(value?: T): [
     s: T,
     setFn: React.Dispatch<React.SetStateAction<T>>,
 ] => {
-    const [state, setState] = useState<T>(value as any);
+    const [state, setState] = useState<T>(value as T);
     const setCb = useSafeCb(setState)
     return [state, setCb];
 }
