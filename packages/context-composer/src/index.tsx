2038cc45ec11810e2083a0f7d5adff19a65a878f
apathyjade <apathyjade@outlook.com>
2025-02-21 00:41:03 +0800

    feat: 优化context-composer

--- packages/context-composer/src/index.tsx
+++ packages/context-composer/src/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { FC } from 'react';
+
+
+type Composer<P> = (Com: FC<P>) => FC<P>;
 
 export const createCtxComposer = <P extends React.JSX.IntrinsicAttributes, T>(useCfgHook: (props: P) => T) => {
   const ComposerCtx = React.createContext<T>({} as any);
   return {
-    forwardComposer: (Com: (props: P) => React.ReactNode): (props: P) => React.ReactNode => (props: P) => {
+    forwardComposer: (Com: FC<P>): FC<P> => (props: P) => {
       const value = useCfgHook(props);
       return (
         <ComposerCtx.Provider value={value}>
@@ -18,13 +21,14 @@ export const createCtxComposer = <P extends React.JSX.IntrinsicAttributes, T>(us
 };
 
 export const mergeComposer = <P extends React.JSX.IntrinsicAttributes>(
-  list: Array<(Com: (props: P) => React.ReactNode) =>(props: P) => React.ReactNode>,
-  Com: (props: P) => React.ReactNode
-): (props: P) => React.ReactNode => {
-  return list.reduce((res, it) => {
-    return (props: P) => {
-      const CurrCom = it(res);
-      return <CurrCom {...props} />
-    };
-  }, Com)
-}
+  list: Array<Composer<P>>,
+): Composer<P> => {
+  return (Com: FC<P>): FC<P> => {
+    return list.reduce((res, it) => {
+      return (props: P) => {
+        const CurrCom = it(res);
+        return <CurrCom {...props} />;
+      };
+    }, Com);
+  };
+};
